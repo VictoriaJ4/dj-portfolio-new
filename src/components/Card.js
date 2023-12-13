fd404150ae96ecd5f4e06b4f737bff5f7c15255d
victoria juszkiewicz <wanderer.foxy@gmail.com>
2023-12-13 11:39:36 +0100

    card fixed

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,16 +14,17 @@ const Card = ({ id,imageUrl, title, text }) => {
 
 	return (
 		<div className="card" style={{ width: "18rem" }} onClick={handleCardClick}>
-			 <Link to={`/news/${id}`}>
+		
 			<img src={imageUrl} className="card-img-top" alt="..." />
 			<div className="card-body">
 				<h5 className="card-title">{title}</h5>
 				<p className="card-text">{text}</p>
+				<Link to={`/news/${id}`}>
 				 <a href="#" className="btn btn-primary">
           Read more
         </a>
-		
-			</div></Link>
+		</Link>
+			</div>
 		</div>
 	);
 };
